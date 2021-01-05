ee5e255ec4784a82bb79757923d9b5310b3c9dd8
Kim Blanck <kimblanck@kims-air.home>
2021-01-05 15:26:04 -0500

    took out /writers folder

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fullstackapp
+DECOLONIZE MY AUDITION BOOK was built using HTML, CSS, JavaScript, Express, Node, Mongo, and MongoDB Atlas. Deployed on Heroku, it can be found at 
+
--- controllers/writers.js
+++ controllers/writers.js
@@ -4,23 +4,23 @@ const Writer = require('../models/writers.js');
 const seed = require('../models/seed.js');
 
 // SEED ROUTE
-router.get('/writers/seed', (req, res) => {
+router.get('/seed', (req, res) => {
     Writer.create(seed, (error, data) => {
-        res.redirect('/writers')
+        res.redirect('/')
     })
 });
 
 // CREDITS PAGE
-router.get('/writers/credits', (req, res) => {
+router.get('/credits', (req, res) => {
     res.render('credits.ejs')
 });
 
 //CREATE ROUTE
-router.get('/writers/new', (req, res) => {
+router.get('/new', (req, res) => {
     res.render('new.ejs')
 });
 
-router.post('/writers', (req, res) => {
+router.post('/', (req, res) => {
     try {if(req.body.notableAwards) {
         req.body.notableAwards = true
       } else {
@@ -29,12 +29,12 @@ router.post('/writers', (req, res) => {
         req.body.notableAwards = false
       }
     Writer.create(req.body, (error, createdWriter) => {
-        res.redirect('/writers')
+        res.redirect('/')
     })
 }); 
 
 // INDICES
-router.get('/writers', (req, res) => {
+router.get('/', (req, res) => {
     Writer.find({}, (error, allWriters) => {
         res.render('index.ejs', {
             writers: allWriters
@@ -42,7 +42,7 @@ router.get('/writers', (req, res) => {
     })
 });
 
-router.get('/writers/index_black_writers', (req, res) => {
+router.get('/index_black_writers', (req, res) => {
     Writer.find({ identity: 'Black' }, (error, blackWriters) => {
         res.render('index_black_writers.ejs', {
             writers: blackWriters
@@ -50,7 +50,7 @@ router.get('/writers/index_black_writers', (req, res) => {
     })
 });
 
-router.get('/writers/index_asian_writers', (req, res) => {
+router.get('/index_asian_writers', (req, res) => {
     Writer.find({ identity: 'Asian' },(error, asianWriters) => {
         res.render('index_asian_writers.ejs', {
             writers: asianWriters
@@ -59,7 +59,7 @@ router.get('/writers/index_asian_writers', (req, res) => {
 });
 
 
-router.get('/writers/index_indigenous_writers', (req, res) => {
+router.get('/index_indigenous_writers', (req, res) => {
     Writer.find({ identity: 'Indigenous'}, (error, indigenousWriters) => {
         res.render('index_indigenous_writers.ejs', {
             writers: indigenousWriters
@@ -67,7 +67,7 @@ router.get('/writers/index_indigenous_writers', (req, res) => {
     })
 });
 
-router.get('/writers/index_latinx_writers', (req, res) => {
+router.get('/index_latinx_writers', (req, res) => {
     Writer.find({identity: 'Latinx'}, (error, latinxWriters) => {
         res.render('index_latinx_writers.ejs', {
             writers: latinxWriters
@@ -75,7 +75,7 @@ router.get('/writers/index_latinx_writers', (req, res) => {
     })
 });
 
-router.get('/writers/index_menasa_writers', (req, res) => {
+router.get('/index_menasa_writers', (req, res) => {
     Writer.find({identity: 'MENASA'}, (error, menasaWriters) => {
         res.render('index_menasa_writers.ejs', {
             writers: menasaWriters
@@ -84,14 +84,14 @@ router.get('/writers/index_menasa_writers', (req, res) => {
 });
 
 // DELETE ROUTE
-router.delete('/writers/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     Writer.findByIdAndRemove(req.params.id, (error, data) => {
-        res.redirect('/writers');
+        res.redirect('/');
     })
 });
 
 // SHOW ROUTE
-router.get('/writers/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     Writer.findById(req.params.id, (error, foundWriter) => {
         res.render('show.ejs', {
             writer: foundWriter
@@ -100,7 +100,7 @@ router.get('/writers/:id', (req, res) => {
 });
 
 // EDIT ROUTE
-router.get('/writers/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
     Writer.findById(req.params.id, (error, foundWriter) => {
         res.render(
             'edit.ejs',
@@ -111,14 +111,14 @@ router.get('/writers/:id/edit', (req, res) => {
     })
 });
 
-router.put('/writers/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     if (req.body.notableAwards === '2020 Pulitzer Prize for Drama') {
         req.body.notableAwards = true;
       } else {
         req.body.notableAwards = false;
       }
     Writer.findByIdAndUpdate(req.params.id, req.body, (error, updatedModel) => {
-        res.redirect('/writers/:id')
+        res.redirect('/:id')
     })
 });
 
